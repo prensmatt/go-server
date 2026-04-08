b33c05d52eb2ce25668a918bb3c729e5f8b6fa5c
Maleyka Heybatova <6nisan2005@gmail.com>
2026-04-08 17:49:40 +0400

    module creation, creating handlers, deletion of javascript codes

--- main.go
+++ main.go
@@ -6,13 +6,47 @@ import(
 	"log"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm();err != nil{
+		fmt.Fprintf(w,"ParseForm() err: %v",err)
+		return
+	}
+	fmt.Fprintf(w,"POST request successful")
+	firstName := r.FormValue("firstName")
+	lastName := r.FormValue("lastName")
+	email := r.FormValue("email")
+	phone := r.FormValue("phone")
+	subject := r.FormValue("subject")
+	message := r.FormValue("message")
+
+	if firstName == "" || lastName == "" || subject == "" || message == "" {
+        http.Error(w, "please fill in all required fields", http.StatusBadRequest)
+        return
+    }
+
+    fmt.Fprintf(w, "FirstName: %s\nLasttName: %s\nEmail: %s\nPhone: %s\nSubject: %s\nMessage: %s", firstName,lastName,email,phone, subject, message)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/home"{
+		http.Error(w, "404 not found",http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET"{
+		http.Error(w,"method is not supported",http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w,"home")
+}
+
 // fileServer for getting all files from static folder
 func main(){
-	fileServer := http.FileServer(http.Dir("/static"))
+	fileServer := http.FileServer(http.Dir("./static"))
 
 
 	// Serve static files from the root path (HTML, CSS, JS, images)
-	http.Handle("/", fileServer)
+	http.Handle("/", http.StripPrefix("/", fileServer))
 
 	// Handle form submissions at /form
 	http.HandleFunc("/form", formHandler)
